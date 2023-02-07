c24befc193bb28c801dd05595392a74df3668fc8
VIRUXE <flavioaspereira@gmail.com>
2023-02-07 19:27:20 +0000

    compiler ru

---
+++ pawno/pawncc_switcher.py
@@ -0,0 +1,18 @@
+import os
+
+if os.path.exists("pawncc.exe_ru"):
+    os.rename("pawncc.exe", "pawncc.exe_orig")
+    os.rename("pawnc.dll", "pawnc.dll_orig")
+
+    os.rename("pawncc.exe_ru", "pawncc.exe")
+    os.rename("pawnc.dll_ru", "pawnc.dll")
+
+    print("Switched to Russian compiler")
+else:
+    os.rename("pawncc.exe", "pawncc.exe_ru")
+    os.rename("pawnc.dll", "pawnc.dll_ru")
+
+    os.rename("pawncc.exe_orig", "pawncc.exe")
+    os.rename("pawnc.dll_orig", "pawnc.dll")
+
+    print("Switched to Original compiler")
